13e4e4cc8ffe099be4f1df5ccea3c6faf575cbb8
furuhama <furuhama@users.noreply.github.com>
2020-01-02 17:02:48 +0900

    Implement serialize! macro

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ use json_serializable::*;
 fn main() {
     let person = Person::new("furuhama".to_string(), 30);
 
-    println!("{}", String::from_utf8(person.serialize().unwrap()).unwrap());
+    println!("{}", person.serialize().unwrap());
 }
